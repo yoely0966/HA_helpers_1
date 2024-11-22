f6c59261f965015ea2391dbf70ee60e710a95542
Joel Weiss <60310086+yoely0966@users.noreply.github.com>
2024-11-22 00:08:30 -0500

    Update __init__.py

--- custom_components/__init__.py
+++ custom_components/__init__.py
@@ -1,2 +1,25 @@
-async def async_setup(hass, config):
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.helpers.typing import ConfigType
+import logging
+
+DOMAIN = "dst_status"
+
+_LOGGER = logging.getLogger(__name__)
+
+async def async_setup(hass: HomeAssistant, config: ConfigType):
+    """Set up the DST Status integration."""
+    hass.data[DOMAIN] = {}
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up the DST Status integration from a config entry."""
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
+    await hass.config_entries.async_forward_entry_unload(entry, "sensor")
     return True
